b15f0aab5d288414bdf1319be11a5777f6213d7c
mark0maherroufaal-lgtm <mark0maherroufaal@gmail.com>
2026-09-08 11:28:51 +0300

    Add files via upload

--- api/bot.ts
+++ api/bot.ts
@@ -1,3 +1,16 @@
-import { webhookCallback } from "grammy";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { bot } from "../src/bot";
-export default webhookCallback(bot, "http");
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    if (req.method === "POST") {
+        try {
+            await bot.handleUpdate(req.body);
+            res.status(200).send("OK");
+        } catch (e) {
+            console.error(e);
+            res.status(500).send("Error");
+        }
+    } else {
+        res.status(200).send("Bot is alive!");
+    }
+}
